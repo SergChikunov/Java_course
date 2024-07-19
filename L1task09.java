f1f588fab2acbd378ea03e8225eb8d7b3b7c79f5
Sergey Chikunov <consull@outlook.com>
2024-07-19 16:48:49 +0500

    Java занятие 1. Условный оператор if

---
+++ L1task09.java
@@ -0,0 +1,16 @@
+/*
+ * Условный оператор
+ */
+public class L1task09 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c;
+        if (a > b) {
+            c = a;
+        } else {
+            c = b;
+        }
+        System.out.println(c);
+    }
+}
