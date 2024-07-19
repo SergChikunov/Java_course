37deca869343c02ff1e3a9740baf7d7ecf2f423b
Sergey Chikunov <consull@outlook.com>
2024-07-19 17:07:12 +0500

    Java занятие 1. Тернарный оператор на примере минимального из двух значений

---
+++ L1task11.java
@@ -0,0 +1,11 @@
+/*
+ * Тернарный оператор в поиске минимального значения
+ */
+public class L1task11 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int min = a < b ? a : b;
+        System.out.println(min);
+    }
+}
